0f5d335b3dbdebcd7845adf1fcad2311c2a01ce6
Rafiq <rafiq43934l@gmail.com>
2025-11-10 23:24:56 +0700

    pembagian fitur

--- HealthyKost.cpp
+++ HealthyKost.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//pengerjaan projek ini akan bibagi menjadi beberapa bagian 
+
+// Bagian 1: Menu & Navigasi
+
+// Bagian 2: Input Makanan Harian
+
+// Bagian 3: Analisis Pola Makan
+
+// Bagian 4: Sistem Peringatan
+
+// Bagian 5: Penyimpanan & Riwayat
+
+// Fungsi utama
+int main() {
+
+   
+    return 0;
+}
 
