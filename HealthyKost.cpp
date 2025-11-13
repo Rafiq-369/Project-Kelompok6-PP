7fe16bf666589ce2f9ac47c38a89ddfa457da907
Irfan Ahmad Saputra <irfanahmadsaputra09@gmail.com>
2025-11-13 13:34:39 +0700

    Implement history management functions
    Add functions to manage history: add, display, and delete.

--- HealthyKost.cpp
+++ HealthyKost.cpp
@@ -14,6 +14,33 @@ using namespace std;
 // Bagian 4: Sistem Peringatan
 
 // Bagian 5: Penyimpanan & Riwayat
+void tambahHistory(const string &isi) {
+ofstream file(".txt", ios::app)
+if (!file) {
+cout << "Gagal membuka file" << endl;
+return;
+}
+file << isi << endl;
+file.close();
+}
+
+void tampilHistory() {
+ifstream file(".txt");
+if (!file) {
+cout << "Belum ada history" << endl;
+return;
+}
+string line;
+while (getline(file, line)) {
+cout << line << endl;
+}
+file.close();
+}
+
+void hapusHistory() {
+ofstream file(".txt", ios::trunc);
+file.close();
+}
 
 // Fungsi utama
 int main() {
